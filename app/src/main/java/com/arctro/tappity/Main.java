4fdae878ce2057a2440ef7517d6a9b8b453fdffd
KillerMuffin <benmclean007@gmail.com>
2015-06-11 15:23:03 +1000

    Basic update
    It now works

--- app/src/main/java/com/arctro/tappity/Main.java
+++ app/src/main/java/com/arctro/tappity/Main.java
@@ -2,16 +2,50 @@ package com.arctro.tappity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Main extends Activity {
+
+    String temp_text = "This is a sentence";
+    String[] text;
+
+    int position=-1;
+    int correct=0;
+
+    TextView text_display;
+    EditText text_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text=loadSentence().split("");
+
+        text_display = (TextView) findViewById(R.id.text_display);
+        text_input = (EditText) findViewById(R.id.text_input);
+
+        TextView.OnEditorActionListener text_inputListener = new TextView.OnEditorActionListener(){
+            public boolean onEditorAction(TextView exampleView, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL
+                        && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if(text_input.getText().toString().toLowerCase().equals(getCurrent().toLowerCase())){
+                        correct++;
+                    }
+                    showNext();
+                }
+                return true;
+            }
+        };
+        text_input.setOnEditorActionListener(text_inputListener);
+
+        showNext();
     }
 
 
@@ -32,5 +66,20 @@ public class Main extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showNext(){
+        position++;
+        text_display.setText(text[position]);
+        text_input.setText("");
+    }
+    public String getCurrent(){
+        return text[position];
+    }
+
+    public String loadSentence(){
+        position=0;
+        correct=0;
+        return temp_text;
     }
 }
